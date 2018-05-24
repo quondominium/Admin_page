edf7f23929978870de3923818e161f57bf2dab14
quondominium <quondominium@gmail.com>
2018-05-24 16:50:44 +0300

    added changes to section content

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,6 @@
 // This file will be compiled into app.js and will not be minified.
 // Feel free with using ES6 here.
 
-import DE from './modules/dots';
-
-( ($) => {
-  'use strict';
-
-  // When DOM is ready
-  $(() => {
-    DE.dotsEffect();
-  });
-
-})(jQuery);
+    $(document).ready(function() {
+        $('#summernote').summernote();
+    });
