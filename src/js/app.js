d4a468c65986ec03b87c9f04e96cf39dc22ad09b
quondominium <quondominium@gmail.com>
2018-05-25 10:36:15 +0300

    deletions for section rebuild

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,3 @@
 //
 // This file will be compiled into app.js and will not be minified.
 // Feel free with using ES6 here.
-
-    $(document).ready(function() {
-        $('#summernote').summernote();
-    });
